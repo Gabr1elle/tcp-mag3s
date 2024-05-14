d595550d1cb0589f7540dc1d00a26aad2d993b9f
Jun1orDev <junior.araujo@mag3s.com>
2024-05-13 21:24:05 -0300

    feat: Simplify PWA installation prompt logic
    This commit simplifies the logic for displaying the PWA installation prompt. It removes unnecessary imports and updates the code to use a toast notification with install and cancel actions. The toast notification is only shown when the app is not ready to work offline and there is no need for a refresh.

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,36 +16,23 @@
 
 	<!-- You can use $pwa directly in templates! -->
 	<ClientOnly>
-      <div
-        v-if="$pwa?.offlineReady || $pwa?.needRefresh"
-        class="pwa-toast"
-        role="alert"
-      >
-        <div class="message">
-          <span v-if="$pwa.offlineReady"> App ready to work offline </span>
-          <span v-else>
-            New content available, click on reload button to update.
-          </span>
-        </div>
-        <button v-if="$pwa.needRefresh" @click="$pwa.updateServiceWorker()">
-          Reload
-        </button>
-        <button @click="$pwa.cancelPrompt()">Close</button>
-      </div>
-      <div
-        v-if="
-          $pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh
-        "
-        class="pwa-toast"
-        role="alert"
-      >
-        <div class="message">
-          <span> Install PWA </span>
-        </div>
-        <button @click="$pwa.install()">Install</button>
-        <button @click="$pwa.cancelInstall()">Cancel</button>
-      </div>
-    </ClientOnly>
+		<div
+			v-if="$pwa?.offlineReady || $pwa?.needRefresh"
+			class="pwa-toast"
+			role="alert"
+		>
+			<div class="message">
+				<span v-if="$pwa.offlineReady"> App ready to work offline </span>
+				<span v-else>
+					New content available, click on reload button to update.
+				</span>
+			</div>
+			<button v-if="$pwa.needRefresh" @click="$pwa.updateServiceWorker()">
+				Reload
+			</button>
+			<button @click="$pwa.cancelPrompt()">Close</button>
+		</div>
+	</ClientOnly>
 
 	<UNotifications />
 </template>
@@ -56,7 +43,26 @@ const { $pwa } = useNuxtApp();
 
 const toast = useToast();
 
-onMounted(() => {
+const actions = ref([
+	{
+		label: 'Install',
+		click: () => $pwa.install(),
+	},
+	{
+		label: 'Cancel',
+		click: () => $pwa.cancelInstall(),
+	},
+]);
+
+watchEffect(() => {
+	if ($pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh) {
+		toast.add({
+			title: 'Install PWA',
+			description: 'Install this app on your device for a better experience',
+			timeout: 0,
+			actions,
+		});
+	}
 });
 </script>
 
