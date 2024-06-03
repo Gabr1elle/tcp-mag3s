bea1ea64bff1125e7115a09aa88e9be5ace7696f
jun1orDev <cabral_junior@icloud.com>
2024-06-03 18:13:45 -0300

    feat: Adicionar endpoint para curtir/descurtir post do blog
    Este commit adiciona um novo endpoint para permitir que os usuários curtam ou descurtam um post do blog. O endpoint verifica se o ID do post foi informado e se o usuário está logado. Em seguida, verifica se o usuário já curtiu o post. Se sim, o like é removido. Se não, um novo like é criado. O resultado da operação é retornado como parte da resposta da API. Essa adição melhora a interatividade do sistema, permitindo que os usuários expressem sua opinião sobre os posts do blog.

---
+++ server/api/app/blog/like/[incentiveTokenUser].post.js
@@ -0,0 +1,76 @@
+import { Blog } from '../../../../models/Blog.model';
+
+export default defineEventHandler(async (event) => {
+	const body = await readBody(event);
+
+	//get user id from incentive system
+	const userIncentive = await getUserIncentive(event);
+
+	//verify if the body.postId is empty
+	if (!body.postId) {
+		throw createError({
+			statusCode: 406,
+			message: 'ID do post é obrigatório!',
+			data: null,
+		});
+	}
+
+	//validate the post exists
+	const post = await Blog.Post.findByPk(body.postId);
+	if (!post) {
+		throw createError({
+			statusCode: 406,
+			message: 'Post não encontrado!',
+			data: null,
+		});
+	}
+
+	// get user idIncentive in table user
+	const user = await Blog.User.findOne({
+		where: {
+			incentiveId: userIncentive.id,
+		},
+	});
+
+	//validate the user exists
+	if (!user) {
+		throw createError({
+			statusCode: 406,
+			message: 'Usuário não encontrado!',
+			data: null,
+		});
+	}
+
+	//validate the like exists
+	let like = await Blog.Like.findOne({
+		attributes: ['id', 'postId'],
+		where: {
+			userId: user.id,
+			postId: post.id,
+		},
+	});
+
+	//if like exists, delete it
+	if (like) {
+		await like.destroy();
+
+		return {
+			statusCode: 200,
+			message: 'Você deu deslike 😢!',
+			data: like,
+		};
+	} else {
+		//if like not exists, create it
+		like = await Blog.Like.create({
+			userId: user.id,
+			postId: post.id,
+		});
+
+		return {
+			statusCode: 200,
+			message: 'Você deu like 🤙!',
+			data: like,
+		};
+	}
+
+});
