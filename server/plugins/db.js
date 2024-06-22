ed3c72f17252527512747b4c58ddac6713ea2539
jun1orDev <cabral_junior@icloud.com>
2024-06-21 22:08:15 -0300

    feat: Adicionar agendamento de limpeza do banco de dados de logs do sistema

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,3 +1,7 @@
+import { schedule } from 'node-cron';
+import { Op } from 'sequelize';
+import { Admin } from '../models/Admin.model';
+
 const config = useRuntimeConfig();
 
 export default defineNitroPlugin(async (nitro) => {
@@ -13,4 +17,19 @@ export default defineNitroPlugin(async (nitro) => {
 	} catch (err) {
 		throw new Error(`Não foi possível conectar ao banco: ${err}`);
 	}
+
+	schedule('0 0 0 1 * *', async () => { // Schedule the task to run every 1st day of the month at 00:00:00
+		logger.info('Executando a limpeza do banco de dados de logs do sistema...');
+		try {
+			await Admin.SystemLog.destroy({
+				where: {
+					createdAt: {
+						[Op.lt]: new Date(new Date().setMonth(new Date().getMonth() - 1)), // Delete logs older than 1 month
+					},
+				},
+			});
+		} catch (err) {
+			logger.error(`Erro ao limpar o banco de dados de logs: ${err}`);
+		}
+	});
 });
